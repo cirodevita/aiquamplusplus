f2130bc53d2f0e6adc3fa6171ef11d4657563351
cirodevita <ciro.devita001@gmail.com>
2024-07-23 19:16:04 +0200

    Updaded script to support macOS date format

--- scripts/download.sh
+++ scripts/download.sh
@@ -2,7 +2,7 @@
 
 start_date=""
 base_url=""
-output_folder=""
+output_folder="inputs/"
 
 # Check if the input date format is valid
 if [[ ! $start_date =~ ^[0-9]{8}Z[0-9]{4}$ ]]; then
@@ -13,8 +13,17 @@ fi
 # Convert input date to human-readable format for date command
 start_date_formatted=$(echo "$start_date" | sed -e 's/\(....\)\(..\)\(..\)Z\(..\)/\1-\2-\3 \4:00:00/')
 
-# Convert input date to epoch time
-input_epoch=$(date -u -d "$start_date_formatted" +"%s")
+# Determine the operating system
+os_type=$(uname)
+
+# Convert input date to epoch time based on the OS
+if [[ "$os_type" == "Darwin" ]]; then
+    # macOS
+    input_epoch=$(date -u -j -f "%Y-%m-%d %H:%M:%S" "$start_date_formatted" +"%s")
+else
+    # Linux
+    input_epoch=$(date -u -d "$start_date_formatted" +"%s")
+fi
 
 # Check if the input date is valid
 if [ $? -ne 0 ]; then
@@ -27,19 +36,35 @@ for ((i=0; i<=73; i++)); do
     # Calculate the epoch time for each hour
     previous_epoch=$((input_epoch - (i * 3600)))
     # Convert the epoch time back to human-readable format
-    previous_date=$(date -u -d "@$previous_epoch" +"%Y%m%dZ%H")
-
-    # Extract year, month, day, and hour
-    year=$(date -u -d "@$previous_epoch" +"%Y")
-    month=$(date -u -d "@$previous_epoch" +"%m")
-    day=$(date -u -d "@$previous_epoch" +"%d")
-    hour=$(date -u -d "@$previous_epoch" +"%H")
+    if [[ "$os_type" == "Darwin" ]]; then
+        # macOS
+        previous_date=$(date -u -r "$previous_epoch" +"%Y%m%dZ%H")
+        year=$(date -u -r "$previous_epoch" +"%Y")
+        month=$(date -u -r "$previous_epoch" +"%m")
+        day=$(date -u -r "$previous_epoch" +"%d")
+        hour=$(date -u -r "$previous_epoch" +"%H")
+    else
+        # Linux
+        previous_date=$(date -u -d "@$previous_epoch" +"%Y%m%dZ%H")
+        year=$(date -u -d "@$previous_epoch" +"%Y")
+        month=$(date -u -d "@$previous_epoch" +"%m")
+        day=$(date -u -d "@$previous_epoch" +"%d")
+        hour=$(date -u -d "@$previous_epoch" +"%H")
+    fi
 
     # Construct the file URL
     file_url="${base_url}/${year}/${month}/${day}/wcm3_d03_${previous_date}00.nc"
 
     # echo \"wcm3_d03_${previous_date}00.nc\",
     
-    # Download the file
-    wget -P "$output_folder" "$file_url"
+    # Construct the file name
+    file_name="wcm3_d03_${previous_date}00.nc"
+    
+    # Check if the file already exists
+    if [[ -f "${output_folder}/${file_name}" ]]; then
+        echo "File ${file_name} already exists. Skipping download."
+    else
+        # Download the file if it doesn't exist
+        wget -P "$output_folder" "$file_url"
+    fi
 done
